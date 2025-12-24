d84fe1a2a6d1992f17e471b827e1acdc5a6196e5
Michael Migliore <mcmigliore@gmail.com>
2025-12-24 17:26:44 +0100

    update with f3d master

--- app/src/main/java/app/f3d/F3D/android/Utils/FileType.java
+++ app/src/main/java/app/f3d/F3D/android/Utils/FileType.java
@@ -11,26 +11,4 @@ public class FileType {
             "application/octet-stream",      // .3ds, .glb, .mhd, .nrrd, .obj, .ply, .pts, .vtk, .vtu, .vtp, .vti, .vtr, .vts, .vtm
             "application/vnd.ms-pki.stl",    // .stl,
     };
-    public static boolean checkFileTypeMethod(String fileType){
-
-        String[] fileTypesUsingGeometry = {
-                "obj",
-                "ply",
-                "pts",
-                "stl",
-                "vtk",
-                "vtu",
-                "vtp",
-                "vti",
-                "vtr",
-                "vts",
-                "dcm",      // -> Doubts and needs Volume rendering
-                "mhd",      // -> Doubts and needs Volume rendering
-                "nrd",      // -> Doubts and needs Volume rendering
-                "tif",      // -> Doubts and needs Volume rendering
-                "vtm"       // -> Doubts and needs Volume rendering
-        };
-
-        return Arrays.asList(fileTypesUsingGeometry).contains(fileType);
-    }
 }
