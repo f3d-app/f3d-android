1c9203bc4a27951ccb719b8045eca06ff9a6487b
Michael Migliore <mcmigliore@gmail.com>
2026-02-19 14:16:32 +0100

    filetypes

--- f3d/src/main/java/app/f3d/F3D/android/Utils/FileType.kt
+++ f3d/src/main/java/app/f3d/F3D/android/Utils/FileType.kt
@@ -2,10 +2,8 @@ package app.f3d.F3D.android.Utils
 
 object FileType {
     val supportedMimeTypes: Array<String> = arrayOf<String>(
-        "image/tiff",  // .tif,
-        "x-world/x-vrml",  // .wrl,
         "application/dicom",  // .dcm,
-        "application/octet-stream",  // .3ds, .glb, .mhd, .nrrd, .obj, .ply, .pts, .vtk, .vtu, .vtp, .vti, .vtr, .vts, .vtm
+        "application/octet-stream",  // .3ds, .glb, .obj, .ply, .pts, .vtk, .vtu, .vtp, .vti, .vtr, .vts, .vtm
         "application/vnd.ms-pki.stl",  // .stl,
     )
 }
