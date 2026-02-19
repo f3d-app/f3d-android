419c83d0378efb0651c90aa81f4a8ca68b7bb793
Michael Migliore <mcmigliore@gmail.com>
2026-02-19 14:20:35 +0100

    simplify

--- f3d/src/main/java/app/f3d/F3D/android/Point.kt
+++ f3d/src/main/java/app/f3d/F3D/android/Point.kt
@@ -3,13 +3,8 @@ package app.f3d.F3D.android
 /**
  * Provides a simple implementation of a two-dimensional point with floats.
  */
-class Point @JvmOverloads constructor(xPosition: Float = 0.0f, yPosition: Float = 0.0f) {
+class Point(xPosition: Float = 0.0f, yPosition: Float = 0.0f) {
 
-    var x: Float
-    var y: Float
-
-    init {
-        this.x = xPosition
-        this.y = yPosition
-    }
+    var x: Float = xPosition
+    var y: Float = yPosition
 }
